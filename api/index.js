f5ebf4059c33bc687a245d821beccb6283ab00db
webdev2agency <webdev2agency@gmail.com>
2026-02-25 22:53:50 +0100

    modified:   api/index.js 	modified:   vercel.json

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const app = require('../backend/server');
 
-// This bridge allows Vercel to see the backend as a standard serverless function
-// while keeping all your backend code in the "backend" folder.
-
-module.exports = app;
+// Bridge entry for Vercel Serverless Functions
+module.exports = (req, res) => {
+    // Add bridge-level logging
+    console.log(`[Bridge] ${req.method} ${req.url}`);
+    return app(req, res);
+};
